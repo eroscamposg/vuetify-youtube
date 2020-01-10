2d26ca607ee39629ff0eae52f00f28346bd72d55
Eros Campos <eroscamposg@gmail.com>
2020-01-09 20:41:49 -0500

    Dialog added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -63,11 +63,30 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi dignissimos vitae minima? Eveniet quas veritatis consequuntur asperiores tempora quis cumque nobis sint voluptatem similique eligendi cum, eius, debitis quidem non.</p>
           </v-card-text>
           <v-card-actions>
-            <v-btn color="info" flat="true">Acción</v-btn>
+            <v-btn color="info" flat="true" @click="dialog = true ">Acción</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
+
+    <!-- DIALOG. Persistent, solo haciendole click al boton pueden salir del dialog-->
+    <v-dialog
+      v-model="dialog"
+      scrollable
+      persistent
+      :overlay="false"
+      max-width="500px"
+      transition="dialog-transition"
+    >
+      <v-card>
+        <v-card-title primary-title>Title</v-card-title>
+        <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque explicabo modi perferendis numquam. Suscipit, ab minima? Praesentium iusto saepe sed quo, minus facilis iure earum odio ad quaerat aut similique!</v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success" @click="dialog = false">Accion</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -76,7 +95,12 @@
 
 export default {
   name: "home",
-  components: {}
+  components: {},
+  data() {
+    return {
+      dialog: true
+    };
+  }
 };
 </script>
 
